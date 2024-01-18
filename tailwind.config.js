69de7bab9c1d34933e306dd8dcf087fc59d61b29
fernandogbz <fgjv06@gmail.com>
2024-01-18 17:39:24 -0300

    Configuration to add tailwind to index.html and all the files with the extension in js, tx, jsx and tsx in src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,tx,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
