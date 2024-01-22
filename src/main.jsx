1de13dedfecbc9aee1c81e352ecc945199537864
fernandogbz <fgjv06@gmail.com>
2024-01-22 18:43:22 -0300

    const router calls createBrowserRouter() function

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider />
