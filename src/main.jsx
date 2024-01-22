fde894b4cc2d98c7abdfe45f99d3d96565e1ca94
fernandogbz <fgjv06@gmail.com>
2024-01-22 18:47:04 -0300

    This function 'createBrowserRouter' takes an array in which you define your different router through an object

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter()
+const router = createBrowserRouter([
+  {
+
+  }
+]) // This function takes an array in which you define your different router through an object.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
