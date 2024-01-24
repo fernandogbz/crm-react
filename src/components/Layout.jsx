f22098159baab661849c2f3cb0f04536067a10bb
fernandogbz <fgjv06@gmail.com>
2024-01-24 17:12:43 -0300

    div blue, md1/4 and paddings

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,7 @@ import {Outlet} from 'react-router-dom'
 function Layout() {
   return (
     <div className='md:flex md:min-h-screen'>
+      <div className='md:w-1/4 bg-blue-600 px-5 py-10'></div>
     </div>
   )
 }
