819b9b4053bfb0d5e6e3ee3e97299e5850a59a6a
fernandogbz <fgjv06@gmail.com>
2024-01-25 19:02:06 -0300

    import useLocation, location hook added. And condition in Link component depending on what location.pathname the user is, to highlight the route with different style (white letters)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,19 @@
-import {Outlet, Link} from 'react-router-dom'
+import {Outlet, Link, useLocation} from 'react-router-dom'
 
 function Layout() {
+
+  const location = useLocation()
+
+  console.log(location)
+
   return (
     <div className='md:flex md:min-h-screen'>
       <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
         <h2 className='text-4xl font-black text-center text-white'>CRM - Clients</h2>
         <nav className='mt-10'>
           
-          <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/'>Clients</Link>
-          <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/clients/new'>New Client</Link>
+          <Link className={`${location.pathname === '/' ? 'hover:text-blue-300' : 'text-white'}text-2xl block mt-2 hover:text-blue-300`} to='/'>Clients</Link>
+          <Link className={`${location.pathname === '/clients/new' ? 'hover:text-blue-300' : 'text-white'}text-2xl block mt-2 hover:text-blue-300`} to='/clients/new'>New Client</Link>
         </nav>
       </aside>
 
