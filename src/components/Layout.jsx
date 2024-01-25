75b3965a5339f8663cfdb118602800e91508f4b7
fernandogbz <fgjv06@gmail.com>
2024-01-25 18:10:10 -0300

    Link component added inside nav tag, both with styles classes. Link allow the user navigate among different pages, optimizing the loading time

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,6 +6,7 @@ function Layout() {
       <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
         <h2 className='text-4xl font-black text-center text-white'>CRM - Clients</h2>
         <nav className='mt-10'>
+          
           <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/'>Clients</Link>
           <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/clients/new'>New Client</Link>
         </nav>
