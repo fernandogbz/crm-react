4d4778516ee8436d34b1d08f36bd131cab17923a
fernandogbz <fgjv06@gmail.com>
2024-01-31 19:54:42 -0300

    return from Loader and comments

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,8 @@
 export function loader(){
-  
-}
+
+  return "From Loader"
+
+} // This loader is very similar to a useEffect. It's not exactly the same, but it's very similar and it's a function that will be executed when the component load and is ideal for loading a state or for querying some API and getting some result that you want to display in a component.
 
 
 function Index() {
