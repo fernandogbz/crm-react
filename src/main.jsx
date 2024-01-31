1ae2112108216750bd833cf0d596e46d3297a4eb
fernandogbz <fgjv06@gmail.com>
2024-01-31 19:56:35 -0300

    import loader from Index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import NewClient from './pages/NewClient'
-import Index from './pages/Index'
+import Index, { loader } from './pages/Index'
 
 const router = createBrowserRouter([
   {
