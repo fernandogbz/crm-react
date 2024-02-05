2be4b136603f1f126b5dc19add26029223f3f51d
fernandogbz <fgjv06@gmail.com>
2024-02-05 20:13:27 -0300

    Delete button added with edit's same styles

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -14,13 +14,18 @@ function Client({client}) {
                 <p className="text-gray-600"> <span className="text-gray-800 uppercase font-bold">Phone: </span>{phone}</p>
             </td>
 
-            <td className="p-6 flex">
+            <td className="p-6 flex gap-3">
                 <button 
                     type="button"
                     className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
                 >
-                    
                         Edit
+                </button>
+                <button 
+                    type="button"
+                    className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+                >
+                        Delete
                 </button>
             </td>
         </tr>
