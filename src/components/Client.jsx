683d2c053fe7cabdd751d61063fdf1fa5e60b1a2
fernandogbz <fgjv06@gmail.com>
2024-02-05 19:55:30 -0300

    tr styles - border b added

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -2,13 +2,13 @@ function Client({client}) {
 
     const {id, name, phone, email, company} = client;
 
-  return (
-    <tr>
-        <td className="p-6">
-        {client.name}
-        </td>
-    </tr>
-  )
+    return (
+        <tr className="border-b">
+            <td className="p-6">
+                {client.name}
+            </td>
+        </tr>
+    )
 }
 
 export default Client
