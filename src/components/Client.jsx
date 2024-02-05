17063ef2ae7bed5a18a94660254d5ebee011af3d
fernandogbz <fgjv06@gmail.com>
2024-02-05 20:05:30 -0300

    td added for email, with a p for email with styles - text-gray, uppercase, font bold

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -4,9 +4,13 @@ function Client({client}) {
 
     return (
         <tr className="border-b">
-            <td className="p-6">
+            <td className="p-6 space-y-2">
                 <p className="text-2xl text-gray-800">{client.name}</p>
                 <p>{company}</p>
+            </td>
+
+            <td className="p-6">
+                <p className="text-gray-600"> <span className="text-gray-800 uppercase font-bold">Email: </span>{email}</p>
             </td>
         </tr>
     )
