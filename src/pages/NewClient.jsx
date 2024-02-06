a3f3527de206a1cd596ee44d5ad4041add155f62
fernandogbz <fgjv06@gmail.com>
2024-02-06 20:16:57 -0300

    'Complete form' p tag added

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -4,6 +4,7 @@ function NewClient() {
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">New Client</h1>
+      <p className="mt-3">Complete this form to register a new client</p>
     </>
   )
 }
