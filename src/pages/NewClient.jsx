fe578f52a27219030e8673493817641bc025d3e4
fernandogbz <fgjv06@gmail.com>
2024-02-06 20:20:36 -0300

    Back div and button added, with styles, background, text-white, paddings, font bold and uppercase

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -5,6 +5,14 @@ function NewClient() {
     <>
       <h1 className="font-black text-4xl text-blue-900">New Client</h1>
       <p className="mt-3">Complete this form to register a new client</p>
+
+      <div className='flex justify-end'>
+        <button
+        className='bg-blue-800 text-white px-3 py-1 font bold uppercase'
+        >
+          Back
+        </button>
+      </div>
     </>
   )
 }
